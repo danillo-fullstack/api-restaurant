7eb411581dde9caf00c13b1c33414eaa3a4e4891
Danillo Victtor <danilo_victor2004@hotmail.com>
2026-01-28 13:55:36 -0300

    feat: implement product creation with validation using Zod

--- src/routes/products-routes.ts
+++ src/routes/products-routes.ts
@@ -2,8 +2,9 @@ import { ProductController } from "@/controllers/products-controller.js";
 import { Router } from "express";
 
 const productsRoutes = Router();
-const productsController = new ProductController()
+const productsController = new ProductController();
 
 productsRoutes.get("/", productsController.index);
+productsRoutes.post("/", productsController.create);
 
 export { productsRoutes };
